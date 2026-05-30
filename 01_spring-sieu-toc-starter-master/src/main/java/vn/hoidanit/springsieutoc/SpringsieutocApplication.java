002c00940cff1630b0e142cc5d930f6932127ebc
maaitlunghau <chunhau.py@gmail.com>
2026-05-30 20:57:20 +0700

    refactor: remove unused context and bean from main

--- 01_spring-sieu-toc-starter-master/src/main/java/vn/hoidanit/springsieutoc/SpringsieutocApplication.java
+++ 01_spring-sieu-toc-starter-master/src/main/java/vn/hoidanit/springsieutoc/SpringsieutocApplication.java
@@ -2,20 +2,11 @@ package vn.hoidanit.springsieutoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringsieutocApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ac = SpringApplication.run(SpringsieutocApplication.class, args);
-
-		System.out.println("----------------------------------------");
-		System.out.println("Danh sách các Bean đang được quản lý bởi Spring Context:");
-		System.out.println("----------------------------------------");
-		for (String s : ac.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
-		System.out.println("----------------------------------------");
+		SpringApplication.run(SpringsieutocApplication.class, args);
 	}
 }
