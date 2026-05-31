fac404818e51d770478328c4abbc1c5ab76f64bd
maaitlunghau <chunhau.py@gmail.com>
2026-06-01 00:37:09 +0700

    feat(05-spring-security): configure thymeleaf extras dependency in pom.xml

--- 05_spring-security/src/main/java/com/practice/__spring_data_mvc_crud_practice/config/SecurityConfiguration.java
+++ 05_spring-security/src/main/java/com/practice/__spring_data_mvc_crud_practice/config/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration {
                 // .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/").permitAll()
+                        .requestMatchers("/login", "/register").permitAll()
                         .anyRequest().authenticated()
                 )
 
